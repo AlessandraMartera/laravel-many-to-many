4ed75034e418dd521b0c5d18e08f2b6d15d12f75
alessandra martera <ale.martera@gmail.com>
2023-07-25 17:22:57 +0200

    done data in techologytable

--- database/seeders/TechnologyTableSeeder.php
+++ database/seeders/TechnologyTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Project;
 use App\Models\Technology;
 class TechnologyTableSeeder extends Seeder
 {
@@ -15,6 +16,19 @@ class TechnologyTableSeeder extends Seeder
      */
     public function run()
     {
-        Technology::factory()->count(10)->create();
+        $technologies = Technology::factory()->count(10)->create();
+
+        foreach ( $technologies as $technology){
+        $projects = Project :: inRandomOrder() -> limit(rand(1, 5)) -> get();
+
+        $technology -> projects() -> attach($projects);
+        }
+        // $roles = Role :: factory() -> count(10) -> create();
+
+        // foreach ($roles as $role) {
+
+        //     $users = User :: inRandomOrder() -> limit(rand(1, 5)) -> get();
+
+        //     $role -> users() -> attach($users);
     }
 }
